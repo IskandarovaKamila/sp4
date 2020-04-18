522433406243ff21db7279cb2a21c5a2ce6dde82
Kamila <rina29820@gmail.com>
2020-04-17 23:28:12 -0700

    ujhj

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 using namespace std;
+sem_t semaphore;
 
 int main(int argc, char *argv[]){
 	cout<<"Введите интересующий ip-адрес: ";
@@ -14,19 +16,23 @@ int main(int argc, char *argv[]){
 	for(int i=adres.size()-1;adres[i]!='.';i--) //удаление символов до первой точки
 		adres.pop_back();
 
+	sem_init(&semaphore, 0, 1);
+
 	for(int i=1;i<255;i++){
 		string tmp = adres+std::to_string(i);
+		sem_wait(&semaphore);
 		pid_t pr;
 		pr = fork();
 		int status;
 		if(pr == 0){
-		execl("1", "-c 1", (char*) tmp.c_str(), NULL);
+			execl("1", "-c 1", (char*) tmp.c_str(), NULL);
+		}
+		else if (pr > 0){
+			wait(&status);
+		}
+		sem_post(&semaphore);
 	}
-	else if (pr > 0){
-		wait(&status);
-	}
-
-	}
+	sem_destroy(&semaphore);
 	return 0;
 }
 
